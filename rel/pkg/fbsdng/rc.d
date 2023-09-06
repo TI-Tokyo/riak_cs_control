880d63b28b6b32a6e5c0d25e3faf0f82700d65e9
Andrei Zavada <andrei.zavada@tiot.jp>
2023-09-06 14:42:21 +0000

    fbsdng packaging tweaks

--- rel/pkg/fbsdng/rc.d
+++ rel/pkg/fbsdng/rc.d
@@ -8,13 +8,14 @@
 
 . /etc/rc.subr
 
-name=riak-cs-control
+name=riak_cs_control
 command=/usr/local/lib/riak-cs-control/%ERTS_PATH%/bin/beam.smp
 rcvar=riak_cs_control_enable
-start_cmd="/usr/local/bin/riak-cs-control start"
-stop_cmd="/usr/local/bin/riak-cs-control stop"
-pidfile="/var/run/riak-cs-control/riak-cs-control.pid"
+start_cmd="/usr/local/lib/riak-cs-control/bin/riak-cs-control start"
+stop_cmd="/usr/local/lib/riak-cs-control/bin/riak-cs-control stop"
+pidfile="/run/riak-cs-control/riak-cs-control.pid"
 
+riak_cs_control_user=riak_cs_control
 riak_cs_control_env_file="/usr/local/etc/riak-cs-control/config"
 
 load_rc_config $name
